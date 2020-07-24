5cf6a781f623b7eea5e77ceade13a855a375e5f6
Matej <matej.lajh@gmail.com>
2020-07-24 15:14:59 +0200

    Improved endpoint handling
    User can provide URL directly to method or via setUp() method with $this->endpoint.

--- TestUtilities.php
+++ TestUtilities.php
@@ -6,41 +6,54 @@ use Illuminate\Database\Eloquent\Model;
 
 trait TestUtilities
 {
-    protected $endpoint;
+    protected $endpoint = null;
 
-    protected function fieldIsPresent(string $field, string $method = 'POST')
+    protected function fieldIsPresent(string $field, string $endpoint = null, string $method = 'POST')
     {
+        $url = $endpoint ?? $this->endpoint();
+
         $this
-            ->json($method, $this->endpoint, [])
+            ->json($method, $url, [])
             ->assertStatus(422)
             ->assertJsonValidationErrors([$field => 'required']);
 
         $this
-            ->json($method, $this->endpoint, [$field => null])
+            ->json($method, $url, [$field => null])
             ->assertStatus(422)
             ->assertJsonValidationErrors([$field => 'required']);
 
         $this
-            ->json($method, $this->endpoint, [$field => ''])
+            ->json($method, $url, [$field => ''])
             ->assertStatus(422)
             ->assertJsonValidationErrors([$field => 'required']);
     }
 
-    protected function fieldIsUnique(string $field, Model $existing, string $databaseField = null, string $method = 'POST')
+    protected function fieldIsUnique(string $field, Model $existing, string $databaseField = null, string $endpoint = null, string $method = 'POST')
     {
+        $url = $endpoint ?? $this->endpoint();
+
         if (!$databaseField) $databaseField = $field;
 
         $this
-            ->json($method, $this->endpoint, [$field => $existing[$databaseField]])
+            ->json($method, $url, [$field => $existing[$databaseField]])
             ->assertStatus(422)
             ->assertJsonValidationErrors([$field => 'taken']);
     }
 
-    protected function fieldValueIsInvalid(string $field, $invalidValue, string $method = 'POST')
+    protected function fieldValueIsInvalid(string $field, $invalidValue, string $endpoint = null, string $method = 'POST')
     {
+        $url = $endpoint ?? $this->endpoint();
+
         $this
-            ->json($method, $this->endpoint, [$field => $invalidValue])
+            ->json($method, $url, [$field => $invalidValue])
             ->assertStatus(422)
             ->assertJsonValidationErrors([$field => 'invalid']);
+    }
+
+    private function endpoint()
+    {
+        if (!$this->endpoint) throw new Exception('Endpoint not provided.');
+
+        return $this->endpoint;
     }
 }
